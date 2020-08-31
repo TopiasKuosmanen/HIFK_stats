ef9da0cd8d7205da901ebfe9ea62c7b756cf3cd4
TopiasKuosmanen <topias.kuosmanen@hotmail.com>
2020-08-31 17:08:37 +0300

    Procedure for goals

---
+++ Database1/dbo/Stored Procedures/GetGoals.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE [dbo].[GetGoals]
+	@Leagues NVARCHAR(50),
+	@Opponents NVARCHAR(50),
+	@StartingDay DATETIME,
+	@EndingDay DATETIME,
+	@Player NVARCHAR(50),
+	@Winner BIT,
+	@Penalty BIT,
+	@Assist NVARCHAR(50),
+	@Minute INT
+AS
+BEGIN
+IF (@Leagues != '' AND @Opponents != '' AND @Player != '' AND @Assist != '' AND @Minute IS NOT NULL)
+BEGIN
+	SELECT G.Id, G.GameID, P.FirstName, P.LastName, G.Winner, G.Penalty, G.Minute, A.FirstName AS AssistFirstName, A.LastName AS AssistLastName
+FROM Goal G JOIN Player P ON P.Id = G.PlayerId LEFT JOIN (SELECT G.Id, G.GameID, P.FirstName, P.LastName, G.Winner, G.Penalty, G.Minute
+FROM Goal G JOIN Player P ON P.Id = G.AssistId) A ON A.Id = G.Id JOIN Game GA ON GA.Id = G.GameId JOIN Opponent O ON O.Id = GA.OpponentId
+WHERE GA.Serie = @Leagues AND O.Team = @Opponents AND GA.DateTime >= @StartingDay AND GA.DateTime <= @EndingDay AND (P.LastName LIKE '%' + @Player + '%' OR P.FirstName LIKE '%' + @Player + '%') AND 
+(A.LastName LIKE '%' + @Player + '%' OR A.FirstName LIKE '%' + @Player + '%') AND G.Winner = @Winner AND G.Penalty = @Penalty AND G.MINUTE = @Minute
+END
+IF(@Leagues = 'All' AND @Opponents = 'All' AND @Player = '' AND @Assist = '' AND @Minute IS NULL)
+BEGIN
+SELECT G.Id, G.GameID, P.FirstName, P.LastName, G.Winner, G.Penalty, G.Minute, A.FirstName AS AssistFirstName, A.LastName AS AssistLastName
+FROM Goal G JOIN Player P ON P.Id = G.PlayerId LEFT JOIN (SELECT G.Id, G.GameID, P.FirstName, P.LastName, G.Winner, G.Penalty, G.Minute
+FROM Goal G JOIN Player P ON P.Id = G.AssistId) A ON A.Id = G.Id JOIN Game GA ON GA.Id = G.GameId JOIN Opponent O ON O.Id = GA.OpponentId
+WHERE GA.DateTime >= @StartingDay AND GA.DateTime <= @EndingDay AND G.Winner = @Winner AND G.Penalty = @Penalty
+END
+END
--- Database1/dbo/Stored Procedures/GetStatsWithParameters.sql
+++ Database1/dbo/Stored Procedures/GetStatsWithParameters.sql
@@ -10,79 +10,79 @@ IF(@Leagues = 'All' AND @Opponents = 'All' AND @Player = '')
 BEGIN
 	SELECT p.FirstName, p.LastName, SUM(s.Goals) AS Goals, SUM(s.StartingXI + s.SubstitutedIn) AS Games, SUM(s.Assists) AS Assists,
 	   SUM(s.PlayedMinutes) AS PlayedMinutes, SUM(s.StartingXI) AS StartingXI, SUM(s.SubstitutedIn) AS SubstitutedIn,
-	   SUM(s.OnTheBench) AS OnTheBench, SUM(s.YellowCards) AS YellowCards, SUM(s.RedCards) AS RedCards
+	   SUM(s.OnTheBench) AS OnTheBench, SUM(s.YellowCards) AS YellowCards, SUM(s.RedCards) AS RedCards, PlayerId AS PlayerId
 	FROM [Stats] s JOIN Player p ON s.PlayerId = p.Id JOIN Game g ON s.GameId = g.Id
 	WHERE g.DateTime >= @StartingDay AND g.DateTime <= @EndingDay
-	GROUP BY p.FirstName, p.LastName
+	GROUP BY p.FirstName, p.LastName, s.PlayerId
 	ORDER BY Goals DESC
 END
 IF (@Leagues = 'ALL' AND @Opponents != 'All' AND @Player = '')
 BEGIN
 	SELECT p.FirstName, p.LastName, SUM(s.Goals) AS Goals, SUM(s.StartingXI + s.SubstitutedIn) AS Games, SUM(s.Assists) AS Assists,
 	   SUM(s.PlayedMinutes) AS PlayedMinutes, SUM(s.StartingXI) AS StartingXI, SUM(s.SubstitutedIn) AS SubstitutedIn,
-	   SUM(s.OnTheBench) AS OnTheBench, SUM(s.YellowCards) AS YellowCards, SUM(s.RedCards) AS RedCards
+	   SUM(s.OnTheBench) AS OnTheBench, SUM(s.YellowCards) AS YellowCards, SUM(s.RedCards) AS RedCards, PlayerId AS PlayerId
 	FROM [Stats] s JOIN Player p ON s.PlayerId = p.Id JOIN Game g ON s.GameId = g.Id JOIN Opponent o ON g.OpponentId = o.Id
 	WHERE g.DateTime >= @StartingDay AND g.DateTime <= @EndingDay AND o.Team = @Opponents
-	GROUP BY p.FirstName, p.LastName
+	GROUP BY p.FirstName, p.LastName, s.PlayerId
 	ORDER BY Goals DESC
 END
 IF (@Leagues != 'ALL' AND @Opponents = 'All' AND @Player = '')
 BEGIN
 	SELECT p.FirstName, p.LastName, SUM(s.Goals) AS Goals, SUM(s.StartingXI + s.SubstitutedIn) AS Games, SUM(s.Assists) AS Assists,
 	   SUM(s.PlayedMinutes) AS PlayedMinutes, SUM(s.StartingXI) AS StartingXI, SUM(s.SubstitutedIn) AS SubstitutedIn,
-	   SUM(s.OnTheBench) AS OnTheBench, SUM(s.YellowCards) AS YellowCards, SUM(s.RedCards) AS RedCards
+	   SUM(s.OnTheBench) AS OnTheBench, SUM(s.YellowCards) AS YellowCards, SUM(s.RedCards) AS RedCards, PlayerId AS PlayerId
 	FROM [Stats] s JOIN Player p ON s.PlayerId = p.Id JOIN Game g ON s.GameId = g.Id
 	WHERE g.DateTime >= @StartingDay AND g.DateTime <= @EndingDay AND g.Serie = @Leagues
-	GROUP BY p.FirstName, p.LastName
+	GROUP BY p.FirstName, p.LastName, s.PlayerId
 	ORDER BY Goals DESC
 END
 IF (@Leagues != 'ALL' AND @Opponents != 'All' AND @Player = '')
 BEGIN
 	SELECT p.FirstName, p.LastName, SUM(s.Goals) AS Goals, SUM(s.StartingXI + s.SubstitutedIn) AS Games, SUM(s.Assists) AS Assists,
 	   SUM(s.PlayedMinutes) AS PlayedMinutes, SUM(s.StartingXI) AS StartingXI, SUM(s.SubstitutedIn) AS SubstitutedIn,
-	   SUM(s.OnTheBench) AS OnTheBench, SUM(s.YellowCards) AS YellowCards, SUM(s.RedCards) AS RedCards
-	FROM [Stats] s JOIN Player p ON s.PlayerId = p.Id JOIN Game g ON s.GameId = g.Id JOIN Opponent o ON g.OpponentId = o.Id
+	   SUM(s.OnTheBench) AS OnTheBench, SUM(s.YellowCards) AS YellowCards, SUM(s.RedCards) AS RedCards, PlayerId AS PlayerId
+	FROM [Stats] s JOIN Player p ON s.PlayerId = p.Id JOIN Game g ON s.GameId = g.Id JOIN Opponent o ON g.OpponentId = o.Id 
 	WHERE g.DateTime >= @StartingDay AND g.DateTime <= @EndingDay AND g.Serie = @Leagues AND o.Team = @Opponents
-	GROUP BY p.FirstName, p.LastName
+	GROUP BY p.FirstName, p.LastName, s.PlayerId
 	ORDER BY Goals DESC
 END
 IF (@Leagues != 'ALL' AND @Opponents != 'All' AND @Player != '')
 BEGIN
 	SELECT p.FirstName, p.LastName, SUM(s.Goals) AS Goals, SUM(s.StartingXI + s.SubstitutedIn) AS Games, SUM(s.Assists) AS Assists,
 	   SUM(s.PlayedMinutes) AS PlayedMinutes, SUM(s.StartingXI) AS StartingXI, SUM(s.SubstitutedIn) AS SubstitutedIn,
-	   SUM(s.OnTheBench) AS OnTheBench, SUM(s.YellowCards) AS YellowCards, SUM(s.RedCards) AS RedCards
+	   SUM(s.OnTheBench) AS OnTheBench, SUM(s.YellowCards) AS YellowCards, SUM(s.RedCards) AS RedCards, PlayerId AS PlayerId
 	FROM [Stats] s JOIN Player p ON s.PlayerId = p.Id JOIN Game g ON s.GameId = g.Id JOIN Opponent o ON g.OpponentId = o.Id
 	WHERE g.DateTime >= @StartingDay AND g.DateTime <= @EndingDay AND g.Serie = @Leagues AND o.Team = @Opponents AND (LastName LIKE '%' + @Player + '%' OR FirstName LIKE '%' + @Player + '%')
-	GROUP BY p.FirstName, p.LastName
+	GROUP BY p.FirstName, p.LastName, s.PlayerId
 	ORDER BY Goals DESC
 END
 IF (@Leagues != 'ALL' AND @Opponents = 'All' AND @Player != '')
 BEGIN
 	SELECT p.FirstName, p.LastName, SUM(s.Goals) AS Goals, SUM(s.StartingXI + s.SubstitutedIn) AS Games, SUM(s.Assists) AS Assists,
 	   SUM(s.PlayedMinutes) AS PlayedMinutes, SUM(s.StartingXI) AS StartingXI, SUM(s.SubstitutedIn) AS SubstitutedIn,
-	   SUM(s.OnTheBench) AS OnTheBench, SUM(s.YellowCards) AS YellowCards, SUM(s.RedCards) AS RedCards
+	   SUM(s.OnTheBench) AS OnTheBench, SUM(s.YellowCards) AS YellowCards, SUM(s.RedCards) AS RedCards, PlayerId AS PlayerId
 	FROM [Stats] s JOIN Player p ON s.PlayerId = p.Id JOIN Game g ON s.GameId = g.Id
 	WHERE g.DateTime >= @StartingDay AND g.DateTime <= @EndingDay AND g.Serie = @Leagues AND (LastName LIKE '%' + @Player + '%' OR FirstName LIKE '%' + @Player + '%')
-	GROUP BY p.FirstName, p.LastName
+	GROUP BY p.FirstName, p.LastName, s.PlayerId
 	ORDER BY Goals DESC
 END
 IF (@Leagues = 'ALL' AND @Opponents != 'All' AND @Player != '')
 BEGIN
 	SELECT p.FirstName, p.LastName, SUM(s.Goals) AS Goals, SUM(s.StartingXI + s.SubstitutedIn) AS Games, SUM(s.Assists) AS Assists,
 	   SUM(s.PlayedMinutes) AS PlayedMinutes, SUM(s.StartingXI) AS StartingXI, SUM(s.SubstitutedIn) AS SubstitutedIn,
-	   SUM(s.OnTheBench) AS OnTheBench, SUM(s.YellowCards) AS YellowCards, SUM(s.RedCards) AS RedCards
+	   SUM(s.OnTheBench) AS OnTheBench, SUM(s.YellowCards) AS YellowCards, SUM(s.RedCards) AS RedCards, PlayerId AS PlayerId
 	FROM [Stats] s JOIN Player p ON s.PlayerId = p.Id JOIN Game g ON s.GameId = g.Id JOIN Opponent o ON g.OpponentId = o.Id
 	WHERE g.DateTime >= @StartingDay AND g.DateTime <= @EndingDay AND o.Team = @Opponents AND (LastName LIKE '%' + @Player + '%' OR FirstName LIKE '%' + @Player + '%')
-	GROUP BY p.FirstName, p.LastName
+	GROUP BY p.FirstName, p.LastName, s.PlayerId
 	ORDER BY Goals DESC
 END
 IF (@Leagues = 'ALL' AND @Opponents = 'All' AND @Player != '')
 BEGIN
 	SELECT p.FirstName, p.LastName, SUM(s.Goals) AS Goals, SUM(s.StartingXI + s.SubstitutedIn) AS Games, SUM(s.Assists) AS Assists,
 	   SUM(s.PlayedMinutes) AS PlayedMinutes, SUM(s.StartingXI) AS StartingXI, SUM(s.SubstitutedIn) AS SubstitutedIn,
-	   SUM(s.OnTheBench) AS OnTheBench, SUM(s.YellowCards) AS YellowCards, SUM(s.RedCards) AS RedCards
+	   SUM(s.OnTheBench) AS OnTheBench, SUM(s.YellowCards) AS YellowCards, SUM(s.RedCards) AS RedCards, PlayerId AS PlayerId
 	FROM [Stats] s JOIN Player p ON s.PlayerId = p.Id JOIN Game g ON s.GameId = g.Id
 	WHERE g.DateTime >= @StartingDay AND g.DateTime <= @EndingDay AND (LastName LIKE '%' + @Player + '%' OR FirstName LIKE '%' + @Player + '%')
-	GROUP BY p.FirstName, p.LastName
+	GROUP BY p.FirstName, p.LastName, s.PlayerId
 	ORDER BY Goals DESC
 END
