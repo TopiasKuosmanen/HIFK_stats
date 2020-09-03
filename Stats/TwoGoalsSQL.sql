beae2fe4e42abb8f5f1d68d34c348d7920ea55d2
TopiasKuosmanen <topias.kuosmanen@hotmail.com>
2020-09-03 17:06:00 +0300

    Stats

---
+++ Stats/TwoGoalsSQL.sql
@@ -0,0 +1,11 @@
+﻿SELECT COUNT(*) AS 'Kahden maalin ottelut', P.FirstName, P.LastName 
+FROM Stats S JOIN Player P ON S.PlayerId = P.Id
+WHERE Goals IN (2) AND LeagueId IN (1)
+GROUP BY S.PlayerId, P.FirstName, P.LastName
+ORDER BY COUNT(*) DESC
+
+SELECT TOP 5 SUM(S.Goals), P.FirstName, P.LastName
+FROM Stats S JOIN Player P ON S.PlayerId = P.Id
+WHERE S.LeagueId = 1
+GROUP BY S.PlayerId, P.FirstName, P.LastName
+ORDER BY SUM(S.Goals) DESC
